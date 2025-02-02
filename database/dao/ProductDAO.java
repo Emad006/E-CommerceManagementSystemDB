865319b1a972fd2674d56c5769eb5fd506111cb5
Emad <emadharun.eh@gmail.com>
2025-02-02 22:47:46 +0600

    check product existence

--- database/dao/ProductDAO.java
+++ database/dao/ProductDAO.java
@@ -1,5 +1,29 @@
 package database.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import database.connection.DatabaseConnection;
+
 public class ProductDAO {
-    
+
+    public ProductDAO() {}
+
+    // TODO: Validate ID in front-end
+    public boolean productExists(int id) {
+        String fetchProductsQuery = "SELECT PROD_ID FROM PRODUCTS WHERE PROD_ID = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pr = conn.prepareStatement(fetchProductsQuery)) {
+            pr.setInt(1, id);
+            ResultSet rs = pr.executeQuery();
+
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
