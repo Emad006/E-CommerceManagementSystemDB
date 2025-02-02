215025d4afcf8b559657da4c58f77e67ac5b8d2b
Emad <emadharun.eh@gmail.com>
2025-02-02 23:44:21 +0600

    edit product methods

--- database/dao/ProductDAO.java
+++ database/dao/ProductDAO.java
@@ -63,4 +63,42 @@ public class ProductDAO {
     public void addProduct(int id, String name, double price, int stock, String category, String desc) {
         addProduct(id, name, price, stock, category, desc, null);
     }
+
+    // Edit product with provided image path
+    public void editProduct(int id, String name, double price, int stock, String category, String desc, String imagePath) {
+        String editProductQuery;
+
+        // Check if image path is null
+        if (imagePath == null) { // Avoid updating image path
+            editProductQuery = "UPDATE PRODUCTS SET PROD_NAME = ?, PROD_PRICE = ?, PROD_STOCK = ?, PROD_CATEGORY = ?, PROD_DESC = ? WHERE PROD_ID = ?";
+        } else { // Update image path
+            editProductQuery = "UPDATE PRODUCTS SET PROD_NAME = ?, PROD_PRICE = ?, PROD_STOCK = ?, PROD_CATEGORY = ?, PROD_DESC = ?, PROD_IMAGE = ? WHERE PROD_ID = ?";
+        }
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pr = conn.prepareStatement(editProductQuery)) {
+                pr.setString(1, name);
+                pr.setDouble(2, price);
+                pr.setInt(3, stock);
+                pr.setString(4, category);
+                pr.setString(5, desc);
+
+                // Set image path if not null
+                if (imagePath != null) {
+                    pr.setString(6, imagePath);
+                    pr.setInt(7, id);
+                } else {
+                    pr.setInt(6, id);
+                }
+
+                pr.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Edit product with default image
+    public void editProduct(int id, String name, double price, int stock, String category, String desc) {
+        editProduct(id, name, price, stock, category, desc, null);
+    }
 }
